987ea9116a3d7fa852c8fe1b035eb43418cb6293
Ana2020143716 <anacsrodrigues.46@gmail.com>
2023-12-20 00:20:57 +0000

    inicio carrossel

--- js/script.js
+++ js/script.js
@@ -20,3 +20,44 @@ window.addEventListener('scroll', function() {
   // update previous scroll position
   prevScrollPos = currentScrollPos;
 });
+
+
+
+
+
+
+var slidePosition = 1;
+var intervalTime = 3000; // tempo para mudar
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("container2");
+
+  
+  if (slidePosition > slides.length) {
+    slidePosition = 1;
+  }
+
+  if (slidePosition < 1) {
+    slidePosition = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slidePosition - 1].style.display = "inline-block";
+
+  // Increment the slide position for the next iteration
+  slidePosition++;
+
+}
+
+// Call the SlideShow function initially
+SlideShow();
+
+// Set the interval to call the SlideShow function every `intervalTime` milliseconds
+var slideInterval = setInterval(SlideShow, intervalTime);
+
+// If you want to stop the slideshow, you can use clearInterval like this:
+// clearInterval(slideInterval);
